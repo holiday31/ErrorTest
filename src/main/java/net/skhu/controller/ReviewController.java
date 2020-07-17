0f79fe237938136be5f508d1b95d8f21b327cfe2
jimin <holiday31@naver.com>
2020-07-17 16:14:55 +0900

    review image+json

--- src/main/java/net/skhu/controller/ReviewController.java
+++ src/main/java/net/skhu/controller/ReviewController.java
@@ -1,20 +1,66 @@
 package net.skhu.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import net.skhu.domain.Review;
+import net.skhu.domain.ReviewDto;
+import net.skhu.domain.UploadFile;
+import net.skhu.exception.FileUploadException;
+import net.skhu.repository.ReviewRepository;
+import net.skhu.repository.StoreRepository;
+import net.skhu.service.FileUploadDownloadService;
 
 @RestController
 public class ReviewController {
 
-//	@Autowired
-//	private FileUploadDownloadService service;
-//	@Autowired
-//	private ReviewRepository reviewRepository;
-//
-//	@RequestMapping("/review")
-//	public Review review(@RequestParam("photo") MultipartFile photo,@RequestParam("review") Review review) throws FileUploadException {
-//		UploadFile uploadFile = service.storeFile(photo);
-//		review.setPhoto(uploadFile);
-//		Review r=reviewRepository.save(review);
-//		return r;
-//	}
+	@Autowired
+	private FileUploadDownloadService service;
+	@Autowired
+	private ReviewRepository reviewRepository;
+	@Autowired
+	private StoreRepository storeRepository;
+
+	@RequestMapping("/review")
+	public Review review(@RequestParam("photo") MultipartFile photo,@RequestParam("jsonFileVo") String jsonFileVo) throws FileUploadException, org.apache.tomcat.util.http.fileupload.FileUploadException {
+		UploadFile uploadFile = service.storeFile(photo);
+		Review review = new Review();
+		ReviewDto rd = null;
+        try {
+        	 rd= new ObjectMapper().readValue(jsonFileVo, ReviewDto.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        review.setUserId(rd.getUserId());
+        review.setStore(storeRepository.findById(rd.getStore().getId()).get());
+        review.setText(rd.getText());
+        review.setScore(rd.getScore());
+		review.setPhoto(uploadFile);
+		Review r=reviewRepository.save(review);
+		return r;
 }
+
+//		UploadFile uploadFile = service.storeFile(photo);
+//		Review review1 =null;
+//		Review review2 =null;
+//        try {
+//        	 review1= new ObjectMapper().readValue(jsonFileVo, Review.class);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        review2.setUserId(review1.getUserId());
+//        review2.setStore(storeRepository.findById(review1.getStore().getId()).get());
+//        review2.setText(review1.getText());
+//        review2.setScore(review1.getScore());
+//		review2.setPhoto(uploadFile);
+//		reviewRepository.save(review2);
+//		return review2;
+
+}
+
+
---
+++ src/main/java/net/skhu/domain/ReviewDto.java
@@ -0,0 +1,17 @@
+package net.skhu.domain;
+
+import lombok.Data;
+
+@Data
+public class ReviewDto {
+
+	int id;
+	Store store;
+	String userId;
+	String text;
+	float score;
+    UploadFile photo;
+
+//    @JsonIgnore
+//    private MultipartFile file;
+}
